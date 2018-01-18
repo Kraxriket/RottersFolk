d0f91f1f6b1396b98bfed46ea297bb308a3cbecc
Kraxriket <dennis.barthelsson@gmail.com>
2018-01-18 12:36:17 +0100

    Bad commit

---
+++ Assets/Player.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+//TODO
+//Keep player character from sliding or floating down angular terrain due to gravity. Some research required.
+//camera collision and 
+//Set joystick float epsilon for precise controls
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] float Speed = 10f;
+
+    float xThrow, zThrow;
+    public Animator animator;
+    Transform cameraT;
+
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        cameraT = Camera.main.transform;
+        Physics.gravity = new Vector3(0, -40, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessTranslation();
+    }
+
+    void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        zThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(xThrow, 0.0f, zThrow);
+
+        transform.Translate(Quaternion.Euler(0, cameraT.eulerAngles.y, 0) * movement * Speed * Time.deltaTime, Space.World);
+
+        if (movement != Vector3.zero)
+        {
+            Vector3 cameraRelation = (Quaternion.Euler(0, cameraT.eulerAngles.y, 0) * movement);
+            transform.rotation = Quaternion.LookRotation(cameraRelation);
+        }
+
+        float animationSpeedPercent = movement.magnitude;
+        animator.SetFloat("speedPercent", animationSpeedPercent);
+   
+    }
+
+}
+
--- Assets/Scripts/PlayerOLD.cs
+++ Assets/Scripts/PlayerOLD.cs
@@ -9,13 +9,25 @@ using UnityStandardAssets.CrossPlatformInput;
 //camera collision and 
 //Set joystick float epsilon for precise controls
 
-public class Player : MonoBehaviour {
+public class PlayerOLD : MonoBehaviour {
 
     [SerializeField] float Speed = 10f;
+    public float turnSpeed = 1;
 
     float xThrow, zThrow;
     public Animator animator;
     Transform cameraT;
+    public float height = 0.5f;
+    public float heightPadding = 0.05f;
+    public LayerMask ground;
+    public float maxGroundAngle = 120;
+    float angle;
+
+    Quaternion targetRotation;
+
+    Vector3 forward;
+    RaycastHit hitInfo;
+    bool grounded;
 
     // Use this for initialization
     void Start()
@@ -29,27 +41,33 @@ public class Player : MonoBehaviour {
     void Update()
     {
         ProcessTranslation();
+
+
     }
 
+    
 
     void ProcessTranslation()
     {
         xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         zThrow = CrossPlatformInputManager.GetAxis("Vertical");
-
         Vector3 movement = new Vector3(xThrow, 0.0f, zThrow);
 
-        transform.Translate(Quaternion.Euler(0, cameraT.eulerAngles.y, 0) * movement * Speed * Time.deltaTime, Space.World);
+        transform.Translate(movement * Speed * Time.deltaTime, Space.World);
 
         if (movement != Vector3.zero)
         {
-            Vector3 cameraRelation = (Quaternion.Euler(0, cameraT.eulerAngles.y, 0) * movement);
-            transform.rotation = Quaternion.LookRotation(cameraRelation);
+            angle = Mathf.Atan2(xThrow, zThrow);
+            angle = Mathf.Rad2Deg * angle;
+            angle += cameraT.eulerAngles.y;
         }
+
+        targetRotation = Quaternion.Euler(0, angle, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
 
         float animationSpeedPercent = movement.magnitude;
         animator.SetFloat("speedPercent", animationSpeedPercent);
-   
+
     }
 
 }
